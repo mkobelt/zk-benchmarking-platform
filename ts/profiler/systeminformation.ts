e71fbb595c44797a80695765da7a1a9ac52869b2
Max Kobelt <max.kobelt@tuhh.de>
2023-09-18 12:51:32 +0200

    Only record selected system properties

--- ts/profiler/systeminformation.ts
+++ ts/profiler/systeminformation.ts
@@ -1,5 +1,17 @@
 import * as si from "systeminformation";
 
 export default async function collectSytemInformation(): Promise<string> {
-    return JSON.stringify(await si.getAllData(), null, 4);
+    const data = await si.get({
+        "system": "manufacturer, model, version, sku, virtual, virtualHost, raspberry",
+        "bios": "vendor, version, releaseDate, revision, features",
+        "baseboard": "manufacturer, model, version, assetTag, memMax, memSlots",
+        "cpu": "*",
+        "mem": "*",
+        "os": "platform, distro, release, codename, kernel, arch, build, servicepack, uefi",
+        "processes": "all, running, blocked, sleeping, unknown",
+        "currentLoad": "currentLoad",
+        "diskLayout": "type, name, vendor, size, totalCylinders, totalHeads, totalTracks, totalSectors, tracksPerCylinder, sectorsPerTrack, bytesPerSector, firmwareRevision, interfaceType, smartStatus, temperature",
+    });
+
+    return JSON.stringify(data, null, 4);
 }
